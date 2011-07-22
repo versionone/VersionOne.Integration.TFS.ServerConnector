88fd1593326382cc0f7703e9df37633c3c56c3d2
Mikhail <Mikhail@542498e7-56f8-f544-a0f2-a85913ac812a>
2011-07-22 13:08:07 +0000

    code formating replace hardcoded by values from config

--- Filters/Filter.cs
+++ Filters/Filter.cs
@@ -3,13 +3,13 @@ using VersionOne.SDK.APIClient;
 
 namespace VersionOne.ServerConnector.Filters {
     public class Filter {
-
         public string Name { get; set; }
         public FilterActions Operation { get; set; }
         public IList<FilterValue> Values = new List<FilterValue>();
 
         protected internal GroupFilterTerm GetFilter(IAssetType type) {
             var terms = new List<IFilterTerm>();
+
             foreach (var value in Values) {
                 var term = new FilterTerm(type.GetAttributeDefinition(Name));
 
@@ -21,10 +21,8 @@ namespace VersionOne.ServerConnector.Filters {
                         term.NotEqual(value.Value);
                         break;
                 }
-
                 terms.Add(term);
             }
-
             return Operation == FilterActions.And
                 ? (GroupFilterTerm) new AndFilterTerm(terms.ToArray())
                 : new OrFilterTerm(terms.ToArray());
