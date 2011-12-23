dc129e12be44b53572232690bcf0404c93a5fbcf
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2011-12-23 11:55:14 +0000

    support for Username field

--- Entities/Member.cs
+++ Entities/Member.cs
@@ -3,15 +3,20 @@
 namespace VersionOne.ServerConnector.Entities {
     public class Member : Entity {
         public const string EmailProperty = "Email";
+        public const string UsernameProperty = "Username";
         public const string DefaultRoleNameProperty = "DefaultRole.Name";
 
-        public Member() { }
+        protected Member() { }
 
         public Member(Asset asset) : base(asset, null) {}
 
         public string Name {
             get { return GetProperty<string>(NameProperty); }
             set { SetProperty(NameProperty, value); }
+        }
+
+        public string Username {
+            get { return GetProperty<string>(UsernameProperty); }
         }
 
         public string Email {
